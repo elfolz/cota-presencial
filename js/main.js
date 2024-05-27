39c47e812fe7473aa78065c88094378a8d737582
Luiz <lhvalentim@outlook.com>
2024-05-27 18:55:32 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+if (location.protocol.startsWith('https')) {
+	navigator.serviceWorker?.register('service-worker.js')
+	navigator.serviceWorker.onmessage = m => {
+		console.info('Update found!')
+		if (m?.data == 'update') location.reload(true)
+	}
+}
+
+var diasPresenciais = 8
+var diasFerias = 0
+
+function init() {
+	document.querySelector('#diasPresenciais').oninput = e => {
+		diasPresenciais = parseInt(e.target.value)
+		calc()
+	}
+	document.querySelector('#diasFerias').oninput = e => {
+		diasFerias = parseInt(e.target.value)
+		calc()
+	}
+}
+
+function calc() {
+	if (diasPresenciais <= 0) return
+	let diasRestantes = diasPresenciais - Math.round((diasPresenciais * diasFerias) / 30)
+	if (diasRestantes <= 0) diasRestantes = diasPresenciais
+	if (diasFerias > 1 && diasRestantes >= diasPresenciais) return document.querySelector('footer').innerHTML = `Você não precisa ir presencial`
+	document.querySelector('footer').innerHTML = `Você precisa ir presencial ${diasRestantes} vezes`
+}
+
+document.onreadystatechange = () => {
+	if (document.readyState != 'complete') return
+	init()
+}
