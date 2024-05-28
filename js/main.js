a3c71809258991019173c7aafad8ebd6758ecf07
Luiz <lhvalentim@outlook.com>
2024-05-28 16:01:34 -0300

    dias ausentes

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ const data = new Date()
 const totalDiasMes = (new Date(data.getFullYear(), data.getMonth()+1, 0))?.getDate() || 30
 
 var diasPresenciais = parseInt(params.get('dias_presenciais') || '8')
-var diasFerias = 0
+var diasAusentes = 0
 
 function init() {
 	document.querySelector('#diasPresenciais').value = diasPresenciais
@@ -19,17 +19,17 @@ function init() {
 		diasPresenciais = parseInt(e.target.value)
 		calc()
 	}
-	document.querySelector('#diasFerias').oninput = e => {
-		diasFerias = parseInt(e.target.value)
+	document.querySelector('#diasAusentes').oninput = e => {
+		diasAusentes = parseInt(e.target.value)
 		calc()
 	}
 }
 
 function calc() {
-	if (diasPresenciais <= 0 || isNaN(diasPresenciais) || isNaN(diasFerias)) return document.querySelector('footer').innerHTML = ''
-	let diasRestantes = diasPresenciais - Math.round((diasPresenciais * diasFerias) / totalDiasMes)
+	if (diasPresenciais <= 0 || isNaN(diasPresenciais) || isNaN(diasAusentes)) return document.querySelector('footer').innerHTML = ''
+	let diasRestantes = diasPresenciais - Math.round(diasPresenciais * diasAusentes / totalDiasMes)
 	if (diasRestantes <= 0) diasRestantes = diasPresenciais
-	if (diasFerias > 1 && diasRestantes >= diasPresenciais) return document.querySelector('footer').innerHTML = `Você não precisa ir presencial`
+	if (diasAusentes > 1 && diasRestantes >= diasPresenciais) return document.querySelector('footer').innerHTML = `Você não precisa ir presencial`
 	document.querySelector('footer').innerHTML = `Você precisa ir presencial ${diasRestantes} vezes`
 }
 
